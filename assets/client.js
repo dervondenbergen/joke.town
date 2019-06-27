be0da1e9c05090464739d4375746ec54883e070c
Felix De Montis <felix@demont.is>
2019-06-27 05:29:48 +0200

    ddd

--- assets/client.js
+++ assets/client.js
@@ -6,6 +6,14 @@ socket.on("joke", function(joke) {
   showJoke();
 });
 
+socket.on("clear", clear);
+function clear() {
+  nojoke.classList.remove("hidden");
+  votes.classList.add("hidden");
+  newjoke.classList.add("hidden");
+  description.classList.add("hidden");
+}
+
 window.onkeyup = keyboardVote;
 
 function keyboardVote(keyevent) {
@@ -24,12 +32,21 @@ function keyboardVote(keyevent) {
 }
 
 function showJoke() {
+  nojoke.classList.add("hidden");
+  votes.classList.remove("hidden");
+  newjoke.classList.remove("hidden");
+  description.classList.remove("hidden");
+
   totalvotes = currentjoke.votes.down + currentjoke.votes.up;
 
   user_field.textContent = currentjoke.user;
   text_field.textContent = currentjoke.text;
 
   upPercentage = Math.floor((currentjoke.votes.up / totalvotes) * 100);
+
+  if (totalvotes === 0) {
+    upPercentage = 50;
+  }
 
   upvotes_bar.style.width = `${upPercentage}%`;
   downvotes_bar.style.width = `${100 - upPercentage}%`;
